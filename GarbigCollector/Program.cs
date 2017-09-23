3c7cd0026c926e5bfc16ab49368db92155f0fd1d
'MuhammedAshrafElsayed' <'MuhammedAshrafElsayed'>
2017-09-23 15:06:18 +0200

    form1

--- GarbigCollector/Program.cs
+++ GarbigCollector/Program.cs
@@ -16,7 +16,7 @@ namespace GarbigCollector
         {
             // Application.EnableVisualStyles();
             // Application.SetCompatibleTextRenderingDefault(false);
-            // Application.Run(new Form());
+            // Application.Run(new Form1());
             Console.WriteLine("hi");
 
         }
